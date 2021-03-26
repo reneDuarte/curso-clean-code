02e0470de5a3f163a012377e2d7dfa489a01f2b4
Robson S. Rodrigues <Robson@RSR-W10.mshome.net>
2021-03-26 16:27:16 -0300

    Melhoria no exemplo de tratamento de exceções

--- src/main/java/br/com/wmw/cleancode/excecoes/certo/FileUtil.java
+++ src/main/java/br/com/wmw/cleancode/excecoes/certo/FileUtil.java
@@ -2,7 +2,6 @@ package br.com.wmw.cleancode.excecoes.certo;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -23,19 +22,14 @@ public class FileUtil {
 	public String loadFile(final String fileName) {
 		final File fileToRead = new File(fileName);
 		try {
-			final InputStream inputStream = new FileInputStream(fileToRead);
-			try {
+			try (InputStream inputStream = new FileInputStream(fileToRead)) {
 				final StringBuilder conteudo = new StringBuilder();
 				int data;
 				while ((data = inputStream.read()) != -1) {
 					conteudo.append((char) data);
 				}
 				return conteudo.toString();
-			} finally {
-				inputStream.close();
 			}
-		} catch (final FileNotFoundException e) {
-			throw new ArquivoNaoEncontradoException(fileName, e);
 		} catch (final IOException e) {
 			throw new ErroAoLerArquivoException(fileName, e);
 		}
