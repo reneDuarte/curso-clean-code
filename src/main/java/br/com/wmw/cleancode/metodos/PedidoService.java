196f5265b69597688803ece2d0b2460802080028
Renê Celestino Duarte <rene.duarte@wmw.com.br>
2021-03-26 10:48:06 -0300

    .

--- src/main/java/br/com/wmw/cleancode/metodos/PedidoService.java
+++ src/main/java/br/com/wmw/cleancode/metodos/PedidoService.java
@@ -1,25 +1,37 @@
 package br.com.wmw.cleancode.metodos;
 
-import br.com.wmw.cleancode.comentarios.EnunDescontoEstoque;
-import br.com.wmw.cleancode.comentarios.EnunDescontoQuantidade;
+import br.com.wmw.cleancode.comentarios.*;
 
 //TODO: Reduzir a complexidade diminuindo o número de IFs aninhados
 //TODO: Separar a responsabilidade de calcular o desconto da responsabilidade de executar o cálculo do desconto
 public class PedidoService {
 
 	public Double calcularDesconto(final Double valor, final Integer quantidade, final Integer estoque, final boolean descontoPorQuantidade, final boolean descontoPorEstoque) {
-		Double desconto = 0D;
-		if (descontoPorQuantidade) {
-			desconto += EnunDescontoQuantidade.get(quantidade);
-		}
-		if (descontoPorEstoque) {
-			desconto += EnunDescontoEstoque.get(estoque);
-		}
-		return execCalculo(valor , desconto);
+		Pedido pedido = new Pedido();
+		pedido.setValor(valor);
+		pedido.setQuantidade(quantidade);
+		pedido.setEstoque(estoque);
+		pedido.setDescontoPorEstoque(descontoPorEstoque);
+		pedido.setDescontoPorQuantidade(descontoPorQuantidade);
+
+		addPorcentagemDesconto(pedido);
+		setValorLiquidoPedido(pedido);
+		return pedido.geValorLiquido();
 	}
 
-	private Double execCalculo(final Double valor,final Double desconto){
-		return valor * (1 + (desconto / 100));
+	private void addPorcentagemDesconto(Pedido pedido) {
+		if (pedido.isDescontoPorQuantidade()) {
+			new AddPorcentagemDescontoQuantidad().addDesconto(pedido);
+		}
+		if (pedido.isDescontoPorEstoque()) {
+			new AddPorcentagemDescontoPorEstoque().addDesconto(pedido);
+		}
 	}
 
+	private void setValorLiquidoPedido(Pedido pedido){
+		pedido.seValorLiquido(execCalculaDesconto(pedido));
+	}
+	private Double  execCalculaDesconto(Pedido pedido){
+		return pedido.getDesconto() * (1 + (pedido.getDesconto() / 100));
+	}
 }
